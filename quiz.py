86fc42e609a696a1bc9e6a0f55799914c3e1b8d5
meganduffy <meganemilyduffy@gmail.com>
2016-11-08 15:09:02 +0000

    Add function that reads files with simple questions game

---
+++ quiz.py
@@ -0,0 +1,23 @@
+import sys
+
+def get_questions():
+    with open("questions.txt") as f:
+        lines = f.readlines()
+    return [(lines[i], lines[i+1].strip()) for i in range(0, len(lines), 2)]
+
+try:
+    questions = get_questions()
+except IOError as e:
+    print "Error reading questions file: %s." % e
+    sys.exit()
+except IndexError:
+    print "Error: All questions in the file must have answers."
+    sys.exit()
+
+score = 0
+total = len(questions)
+for question, answer in questions:
+    guess = raw_input(question)
+    if guess == answer:
+        score += 1
+print "You got %s out of %s questions right" % (score, total)
