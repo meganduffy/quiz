08df0b38e31e7b57f2c00d1176fc60952cde6eee
meganduffy <meganemilyduffy@gmail.com>
2016-11-09 21:25:58 +0000

    Functioning user driven quiz game with multiple chances on each question

--- quiz.py
+++ quiz.py
@@ -1,23 +1,45 @@
-import sys
+def write_questions():
+    print "Welcome, please add 3 questions and answers to the quiz..."
+    with open("questions.txt", "a") as f:
+        count = 2
+        while count > 0:
+            question = raw_input("Add your question here:")
+            count -= 1
+            if not question:
+                break
+            answer = raw_input("Add the answer to your question here:")
+            if not answer:
+                break
+            f.write(question)
+            f.write("\n")
+            f.write(answer)
+            f.write("\n")
+write_questions()
 
 def get_questions():
     with open("questions.txt") as f:
         lines = f.readlines()
     return [(lines[i], lines[i+1].strip()) for i in range(0, len(lines), 2)]
 
-try:
-    questions = get_questions()
-except IOError as e:
-    print "Error reading questions file: %s." % e
-    sys.exit()
-except IndexError:
-    print "Error: All questions in the file must have answers."
-    sys.exit()
-
+questions = get_questions()
 score = 0
 total = len(questions)
+
+
 for question, answer in questions:
-    guess = raw_input(question)
-    if guess == answer:
-        score += 1
+    count = 1
+    while True:
+        guess = raw_input(question)
+        if guess == answer:
+            score += 1
+            break
+        elif guess != answer and count > 0:
+            count -= 1
+            print "One more try!"
+            continue
+        else:
+            print "...incorrect"
+            break
+
 print "You got %s out of %s questions right" % (score, total)
+
--- quiz_game.py
+++ quiz_game.py
@@ -1,26 +1,26 @@
-import sys
-
 def get_questions():
-    with open("questions.txt", "a+") as f:
-        f.write("\n")
-        f.writelines(raw_input("Add your own question here:"))
-        f.write("\n")
-        f.writelines(raw_input("Add the answer to your question here:"))
-        f.writelines("\n")
+    with open("quiz_game_sample.txt") as f:
         lines = f.readlines()
     return [(lines[i], lines[i+1].strip()) for i in range(0, len(lines), 2)]
 
 questions = get_questions()
-trys = 3
 score = 0
 total = len(questions)
 
 
 for question, answer in questions:
-    guess = raw_input(question)
-    while trys > 0:
+    count = 1
+    while True:
+        guess = raw_input(question)
         if guess == answer:
             score += 1
+            break
+        elif guess != answer and count > 0:
+            count -= 1
+            print "One more try!"
+            continue
         else:
-            trys -= 1
+            print "...incorrect"
+            break
+
 print "You got %s out of %s questions right" % (score, total)
